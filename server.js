de11868265aa1920db3b652e94a8a5ecaf50a3f9
Timothy Oei <timothyoei.dev@gmail.com>
2022-10-24 01:49:04 +0000

    as titled

--- server.js
+++ server.js
@@ -4,12 +4,15 @@
 
 require('dotenv').config();
 
+const dbURL = "mongodb+srv://APIAccess:apiteam123456@cop4331-largeproject-pa.yxoncp7.mongodb.net/?retryWrites=true&w=majority";
+const port = 3001;
+
 const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
 const mongoclient = require('mongodb').MongoClient;
-const client = new mongoclient(process.env.DATABASE_URL);
+const client = new mongoclient(dbURL);
 client.connect();
 
 const bodyParser = require('body-parser');
@@ -18,19 +21,19 @@ const path = require('path');
 
 
 // connect to the database 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(dbURL)
         .then(() => {
             // if all is ok we will be here
             // start the server
             // listen to requests
             console.log('-----in the db and ready to go------');
-            app.listen(process.env.PORT, () => {console.log('-----PORT OPEN---SERVER CHECK Valid------')});
+            app.listen(port, () => {console.log('-----PORT OPEN---SERVER CHECK Valid------')});
         })
         // if we are not here something went wrong
         .catch((errorHere) => {
             console.log(errorHere)
         }); 
 
- const db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('-----DB opening-----successfully connected to the database')); 
